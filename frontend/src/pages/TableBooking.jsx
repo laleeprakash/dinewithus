d9fed5b9771f05599ab50cfb6a39cd93a7e83456
laleeprakash <kasalalaleeprakash@gmail.com>
2025-06-30 21:57:41 +0530

    changed inss

--- frontend/src/pages/TableBooking.jsx
+++ frontend/src/pages/TableBooking.jsx
@@ -5,7 +5,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 function TableBooking() {
-  const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+  const API = import.meta.env.VITE_API_URL;
 
   const { userId, restaurantId } = useParams();
   const navigate = useNavigate();
